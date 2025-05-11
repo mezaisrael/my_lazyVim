4fe15c8f0bf9ad6347159990b49e5994f3889856
israel meza <meza.israel.91@gmail.com>
2025-05-11 16:18:23 -0400

    fix bug with mason and add lorem plugin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
   { "NTBBloodbath/doom-one.nvim" },
   {
+    -- chose doom one as colorscheme
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "doom-one",
---
+++ lua/plugins/mason-workaround.lua
@@ -0,0 +1,5 @@
+return {
+  -- new mason is broken remove when fixed
+  { "mason-org/mason.nvim", version = "1.11.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
+}
